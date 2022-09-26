d617ce5d77b9e32aea059514b716db4b88dc2f35
murat yıldız <mrtyldz118@gmail.com>
2022-09-26 21:25:30 +0100

    First

---
+++ Week1Homeworks/src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci sayısı :";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
